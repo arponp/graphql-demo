be93684cb41f254d9107daa709501814cb9eaf39
Arpon Purkayastha <arponp30@gmail.com>
2022-05-23 13:53:33 -0700

    add Single Book to RootQueryType

--- src/index.ts
+++ src/index.ts
@@ -54,6 +54,16 @@ const RootQueryType = new GraphQLObjectType({
   name: "Query",
   description: "Root Query",
   fields: () => ({
+    book: {
+      type: BookType,
+      description: "Book",
+      args: {
+        id: {
+          type: GraphQLInt,
+        },
+      },
+      resolve: (parent, args) => books.find((book) => (book.id = args.id)),
+    },
     books: {
       type: new GraphQLList(BookType),
       description: "List of All Books",
